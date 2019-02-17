190881261ee0094acdb307ed3b28f2143b93c5aa
tushar.dhuri <tushar.dhuri@capitaassetservices.ie>
2019-02-17 22:49:34 +0000

    1. Solution directory changes.

--- BM.WEB/Startup.cs
+++ BM.WEB/Startup.cs
@@ -23,15 +23,7 @@ namespace BM.WEB
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-
+        {          
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -44,16 +36,19 @@ namespace BM.WEB
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+            //    app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+              //  app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
-
-            app.UseMvc();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
